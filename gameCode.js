89e519f8d98dcc8c4b644fdea3cb0b85959c180a
antonshebalev <anton.shebalev@yandex.ru>
2019-08-30 00:31:58 +0400

    2 задание. Добавлены функции сохранения шага и вывода по номеру шага.            В конце игры вывод.

---
+++ gameCode.js
@@ -0,0 +1,148 @@
+var event, ok;
+
+var questions = [];
+
+    function saveResult (q, a1, a2, corr ){
+        var question = {q:q,a1:a1,a2:a2,correct:corr};
+        return question;
+    }
+    function readSave(itemSaveQ){
+        var question = questions[itemSaveQ]; 
+        for (var i in question)
+            {
+                console.log(i + ": "+ question[i]);
+            }
+    }
+
+    
+                            
+                            
+do {//Выводим первый вопрос
+    ok = false;
+    event = +prompt(works.a00 + works.a1 + works.a2 + '-1 - Выход из игры');
+    if (event == -1) {
+        break;
+    }
+    else {
+       questions.push( saveResult (works.a00 , works.a1 , works.a2, event ));
+        ok = isAnswer(works.a0, event);
+    }
+} while (!ok);
+switch (event) {
+    case 1: // Первое действие  - если в первом окне ввели 1 то открываем серию окон - окно 2
+        do {
+            ok = false;
+            event = +prompt(works.b00 + works.b1 + works.b2 + '-1 - Выход из игры');
+            if (event == -1) {
+                break;
+            }
+            else {
+            questions.push(    saveResult (works.b00 , works.b1 , works.b2, event ));
+                ok = isAnswer(works.b0, event);
+            }
+        } while (!ok);
+        switch (event) {
+            case 1: // Второе действие, если во 2 окне ввели 1 то переходим на 4 окно
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+                  questions.push(      saveResult (works.d00 , works.d1 , works.d2, event ));
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case 2: // Второе действие   Если ввели 2 то также переходим на 4 окно
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+                     questions.push(   saveResult (works.d00 , works.d1 , works.d2, event ));
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case -1: // Второе действие
+                break;
+            default:
+                alert('Ошибка');
+        }
+        break;
+    case 2: // Первое действие    Если в 1 окне ввели 2 то переходим к 3 окну
+        do {
+            ok = false;
+            event = +prompt(works.c00 + works.c1 + works.c2 + '-1 - Выход из игры');
+            if (event == -1) {
+                break;
+            }
+            else {
+               questions.push(  saveResult (works.c00 , works.c1 , works.c2, event ));
+                ok = isAnswer(works.c0, event);
+            }
+        } while (!ok);
+        switch (event) {
+            case 1: // Второе действие
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+                      questions.push(  saveResult (works.d00 , works.d1 , works.d2, event ));
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case 2: // Второе действие
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+                      questions.push(  saveResult (works.d00 , works.d1 , works.d2, event ));
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case -1: // Второе действие
+                break;
+            default:
+                alert('Ошибка');
+        }
+        break;
+    case -1: // Первое действие
+        break;
+    default:
+        alert('Ошибка');
+}
+alert('Спасибо за игру');
+event = +prompt('Выберите сохраненный шаг! Всего их '+ questions.length +'!');
+readSave(event);
+
+//------------------------------------------
+function isAnswer(q, event) {
+    if (isNaN(event) || !isFinite(event)) {
+        alert('Вы ввели недопустимый символ');
+        return false;
+    }
+    else if (event < 1 || event > q) {
+        alert('Ваше число выходит из допустимого диапозона');
+        return false;
+    }
+	return true;
+    
+}
+
